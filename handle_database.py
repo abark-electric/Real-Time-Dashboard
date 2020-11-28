19c3d824ef41c9f9bcce8cec6e1d37b5c1e2b27b
Your Name <you@example.com>
2020-11-27 19:12:34 -0500

    Added functions for drop/add collection

--- handle_database.py
+++ handle_database.py
@@ -1,8 +1,23 @@
 from pymongo import MongoClient
 from constants import MONGODB_CONN
 
-cluster = MongoClient(MONGODB_CONN)
-db = cluster['vip_parser']
+
+class DatabaseController:
+    def __init__(self, database_name, collection_name):
+        self.cluster = MongoClient(MONGODB_CONN)
+        self.db_nm = database_name
+        self.coll_nm = collection_name
+        self.db = self.cluster[database_name]
+        self.coll = self.db[collection_name]
+
+    def create_collection(self):
+        self.db.create_collection(self.coll_nm)
+
+    def drop_collection(self):
+        self.coll.drop()
+
+# cluster = MongoClient(MONGODB_CONN)
+# db = cluster['vip_parser']
 
 # Create collection
 # db.create_collection('ct_data_live')
@@ -12,3 +27,7 @@ db = cluster['vip_parser']
 # Drop collection
 # print('Dropping . . .')
 # collection.drop()
+
+
+if __name__ == "__main__":
+    DatabaseController('vip_parser', 'ct_data_live').drop_collection()
